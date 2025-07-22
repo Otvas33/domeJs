b8d710123348d6bffdbd7ce9a3b6bc5abbbffe72
Otvas33 <jeancharlesotvas@gmail.com>
2025-07-22 16:54:06 +0200

    domeJJs

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,46 @@
 //
 //console.log(getElementsByClassName);
 
+
+// mettre un id  quand on fait un querySelector//
 let texte = document.querySelector("p");
 console.log(texte);
-
+//
 let para = texte.textContent;
-console.log(para); 
+console.log(para);
+
+
+
+           // essai //
+
+let titre = document.querySelector("h1")
+console.log(titre);
+
+let tir = titre.textContent;
+console.log(tir);
+
+// Exercice 2//
+// modifier le texte dans l element qui porte l id element//
+
+let element = document.getElementById("element");
+element.textContent = "changement de texte"
+
+// Exrcice 3 ////
+// ajouter la classe dark que vous aurait cree prealablement defini a l element body de votre html//
+
+let body = document.querySelector("#body");
+console.log(body);
+
+function darkOrLight() {
+    body.classList.add("dark") 
+}
+
+darkOrLight()
+    
+
+
+
+
 
 
 
