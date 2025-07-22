9ff9ba5b9d4700ab829c528174bede667b2dcb81
Otvas33 <jeancharlesotvas@gmail.com>
2025-07-22 15:07:55 +0200

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// manipulation de Dom //
+// grace a document. et a une function native que vous trouver sur internet
+//vous irez me selectionne l element p
+// vous verefire sur un consol log  votre reusite
+// dans une console vous afficheraiz uniquement le contenu de la balise p(cest a dire le texte uniquement le texte)
+
+// Exercice 1 //
+// document.getElementsByClassName("para")
+//
+//console.log(getElementsByClassName);
+
+let texte = document.querySelector("p");
+console.log(texte);
+
+let para = texte.textContent;
+console.log(para);
+
+
+
